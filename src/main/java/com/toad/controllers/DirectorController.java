bae1242781c8f72aaa888858aeb90cc26c8345c1
lsannier <lucas.jl.sannier+portfolio@gmail.com>
2024-10-31 15:46:57 +0100

    Refactored Director controller

--- src/main/java/com/toad/controllers/DirectorController.java
+++ src/main/java/com/toad/controllers/DirectorController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.toad.entities.Director;
-import com.toad.entities.Film;
 import com.toad.repositories.DirectorRepository;
 
 @Controller
