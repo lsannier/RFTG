bb6301902a799a61ae8f90c8943b456b68cc7a78
PierreCHALLES <pch@mtb111.fr>
2025-02-27 09:19:15 +0100

    Modification /add ajout de (Integer.valueOf(0))

--- src/main/java/com/toad/controllers/RentalController.java
+++ src/main/java/com/toad/controllers/RentalController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.toad.entities.Rental;
 import com.toad.repositories.RentalRepository;
 
